8b80b9c2229f8ee6f2b79829a6726265deab7397
mohamed <mohamedboukab202@gmail.com>
2025-03-15 22:09:38 +0000

    Deduct expense from saving amount

--- app/Console/Commands/SubDepense.php
+++ app/Console/Commands/SubDepense.php
@@ -4,6 +4,7 @@ namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
 use App\Models\Depense;
+use App\Models\Epargne;
 use App\Models\User;
 use Carbon\Carbon;
 
@@ -21,15 +22,20 @@ class SubDepense extends Command
 
         foreach ($users as $user) {
             $depenses = Depense::where('user_id', $user->id)->where('type', 'recurrente')->get();
+            $epargne = Epargne::where('user_id', $user->id)->first();
             foreach ($depenses as $depense) {
-                $datePaiement = Carbon::parse($depense->date_paiement);
-                 if ($today == $datePaiement->day) { 
-                    if($user->balance > $depense->montant_depense ) {
-                       $user->balance -= $depense->montant_depense;
-                       $user->save();
-                    }
-                 }       
-            } 
+               $datePaiement = Carbon::parse($depense->date_paiement);
+               if ($today == $datePaiement->day) { 
+                  if($user->balance > $depense->montant_depense ) {
+                     $user->balance -= $depense->montant_depense;
+                     $user->save();
+
+                     $epargne->epargne_total = max(0, $epargne->epargne_total -= $depense->montant_depense);
+                     $epargne->epargne_mensuel = max(0, $epargne->epargne_mensuel -= $depense->montant_depense);
+                     $epargne->save();
+                  }
+               }       
+            }  
         }
   
         $this->info("Le commande de déduction des dépenses est excuté avec succès.");  
